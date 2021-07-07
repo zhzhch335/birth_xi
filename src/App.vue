98aed17e618a27d00490d5165b07b41edbd0c293
zhzhch335 <357611628@qq.com>
2021-07-07 23:34:12 +0800

    初始化

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <HappyBirchdayXi/>
+  </div>
+</template>
+
+<script>
+import HappyBirchdayXi from './components/HappyBirchdayXi'
+
+export default {
+  name: 'App',
+  components: {
+    HappyBirchdayXi
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/HappyBirchdayXi.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="body">
+    <div class="center" @mouseover="showMoasic = true" @mouseleave="showMoasic = false">
+      <img class="main" :style="{'opacity': showMoasic ? 0 : 1}" :src="main" alt=""/>
+      <img class="main" :style="{'opacity': showMoasic ? 1 : 0}" :src="main625" alt=""/>
+      <img class="masoic" :style="{'opacity': showMoasic ? 0.4 : 0.1}" :src="moasic"/>
+      <div v-for="item of grid" v-bind:key="item.id" class="grid"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+import main from "../assets/image/main.png"
+import main625 from "../assets/image/main625.png"
+import mosaic from "../assets/image/result.png"
+export default {
+  name: 'HappyBirchdayXi',
+  data () {
+    let grid = new Array(625).fill([])
+    return {
+      grid,
+      main: main,
+      moasic: mosaic,
+      main625: main625,
+      showMoasic: false
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.center {
+  width: 80vh;
+  height: 80vh;
+  margin: auto;
+  position: relative;
+  top: 0;
+  left: 0;
+  display: flex;
+  flex-wrap: wrap;
+}
+.main {
+  filter: blur(1px);
+  transition: 0.5s all;
+}
+.grid {
+  width: 4%;
+  height: 4%;
+  background: white;
+  opacity: 0.3;
+  position: relative;
+  z-index: 2;
+}
+.grid:hover {
+  background: black;
+  
+}
+.main, .masoic {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
+.masoic {
+  transition: 0.5s all;
+  /* filter: blur(1px) */
+}
+.body {
+  width: 100vw;
+  text-align: center;
+}
+</style>
