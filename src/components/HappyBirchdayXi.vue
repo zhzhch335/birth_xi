74b0bfe3e01b8a875b3330ba32a782fb86f7dd58
zhzhch335 <357611628@qq.com>
2021-07-14 22:51:12 +0800

    修复了部分细节问题，预加载处理

--- src/components/HappyBirchdayXi.vue
+++ src/components/HappyBirchdayXi.vue
@@ -1,10 +1,14 @@
 <template>
   <div class="body">
-    <audio ref="supriseMusic" src="/static/assets/suprise.m4a"></audio>
+    <div v-if="!hideLoading" class="loading">
+      <img class="d-block" src="/static/assets/sunny-light.svg" style="width:96px;opacity:0.5">
+      <div v-if="loading" class="loadingText">加载中，请稍候({{Math.floor(imgloaded / (database.length * 2) * 100)}}%)……</div>
+      <div v-else @click="hideLoading = true" class="loaded">进入</div>
+    </div>
+    <audio ref="supriseMusic" preload="auto" src="/static/assets/suprise.ogg"></audio>
     <!-- <img src="/static/assets/bg2.png" style="z-index:1;position:absolute;right:0;bottom:0" alt=""> -->
-    <iframe v-if="showMusic" id="music" frameborder="no" border="0" marginwidth="0" marginheight="0" width=330 height=86 src="//music.163.com/outchain/player?type=2&id=1385796832&auto=1&height=66"></iframe>
+    <iframe v-if="hideLoading && showMusic" id="music" frameborder="no" border="0" marginwidth="0" marginheight="0" width=330 height=86 src="//music.163.com/outchain/player?type=2&id=1385796832&auto=1&height=66"></iframe>
     <div class="navigator">
-      <div v-if="showSurpirse" @click="startSuprise()" class="li">小希！生日快乐！</div>
       <div @click="toMoeGirl()" class="li">她是谁</div>
       <div @click="showContent = 'imageList'" class="li">二创图列表</div>
       <div @click="showContent = 'blessList'" class="li">祝福列表</div>
@@ -14,23 +18,26 @@
       <div class="supriseBless">
         {{supriseBless}}
       </div>
+      <img v-if="showResetPage" @click="resetPage()" src="/static/assets/reset.png" class="resetIcon" alt="">
       <img class="main" :style="{'opacity': showMoasic ? 0 : 1}" :src="main" alt=""/>
       <img class="main" :style="{'opacity': showMoasic ? 1 : 0}" :src="main625" alt=""/>
       <img class="masoic" :style="{'opacity': showMoasic ? 0.4 : 0.1}" :src="moasic"/>
       <div :style="{'background-image':item ? `url(/static/assets/suprise/图_${index + 1}.gif` : ''}" :class="{'gridRead':item}" @mouseover="showMoasic = true" @mouseleave="showMoasic = false" v-for="(item,index) of grid" @click.stop="clickGrid(index)" v-bind:key="item.id" class="grid"></div>
-      <div class="arrow">
+      <div v-if="!showSurpirse" class="arrow">
         <img src="/static/assets/arrow.png" alt="">
         <div class="suprise">点开每个小图有惊喜哦！</div>
       </div>
-      <div v-if="grid.some(item=>item)" class="btns">
+      <div v-if="grid.some(item=>item) && !showSurpirse" class="btns">
         <div @click.stop="happyBirthDay()" class="btn">懒得翻辣！直接翻开吧！</div>
         <div @click.stop="reset()" class="btn">我要重新翻！</div>
       </div>
     </div>
     <div v-if="currentData" @click="currentData = null" class="blackCover">
+      <!-- <div @click="before()" class="before">❮</div>
+      <div @click="next()" class="next">❯</div> -->
       <div class="postcard">
         <div class="image">
-          <img :src="`/static/${currentData.url}`" alt="">
+          <img @click="showImg(currentData.url)" style="cursor:pointer" :src="`/static/${currentData.url}`" alt="">
           <div class="author" @click.stop="toSpace(currentData.uid)">图：<span :class="currentData.uid ? 'toUid' : ''">{{currentData.author}}</span></div>
         </div>
         <div class="bless" @click.stop="()=>{}">
@@ -39,7 +46,7 @@
         </div>
       </div>
     </div>
-    <div v-if="showContent" @click="showContent = null" class="blackCover">
+    <div v-if="showContent" @click="showContent = null" class="blackCover" style="z-index:7">
       <div class="postcard">
         <div @click.stop="()=>{}" v-if="showContent == 'blessList'" class="blessList">
           <div style="border-bottom: 1px solid rgba(0, 0, 0, 0.5);" class="blessItem">
@@ -68,11 +75,16 @@
             画师 or 表情包工匠
           </div>
           <div class="name">
-            <span>幽灵子辰（没错大图是他画的）、</span>
+            <span>幽灵子辰（没错大图是他画的）、_真名_（标题Q版小希）、凉城暮暖（标题制作）</span>
+          </div>
+          <div class="name">
             <span v-for="author of authorList">{{author.author}}、</span>
           </div>
           <div class="staff">
             虚研会名场面截图
+          </div>
+          <div class="name">
+            （抱歉虽然后来企划变更没有用上……但是感谢你们的付出）
           </div>
           <div class="name">
             时光荏苒、烧煤型驱逐舰、太阳系系王、么鱼、月上弦月下弦、凉城暮暖、诸葛谭所、骑士贝狄威尔
@@ -89,6 +101,7 @@
         </div>
       </div>
     </div>
+    <a id="beian" href="https://beian.miit.gov.cn/" target="_blank">浙ICP备2021021403号-1</a>
   </div>
 </template>
 
@@ -102,7 +115,7 @@ import mosaic from "../assets/image/result.png"
 
 import database from "../assets/database"
 
-function preloadImg(url) {
+function preloadImg(url,cb) {
     var img = new Image();
     img.src = url;
     if(img.complete) {
@@ -113,6 +126,12 @@ function preloadImg(url) {
         img.onload = function() {
             //接下来可以使用图片了
             //do something here
+            cb()
+        };
+        img.onerror = function() {
+            //接下来可以使用图片了
+            //do something here
+            cb()
         };
     }
 }
@@ -123,6 +142,9 @@ export default {
     let grid = JSON.parse(localStorage.getItem('grid')) || new Array(625).fill(false)
     return {
       grid,
+      loading: true,
+      hideLoading: false,
+      imgloaded: 0,
       main: main,
       moasic: mosaic,
       main625: main625,
@@ -134,22 +156,35 @@ export default {
       showContent: null,
       authorList: _.uniqBy(database.filter(item=>item.uid).map(item => { return {author: item.author,uid: item.uid, showImg: false} }), 'uid'),
       imgList: _.uniqBy(database.filter(item=>item.uid).map(item => { return {author: item.author,uid: item.uid} }), 'uid'),
-      showMusic: false,
       showSurpirse: false,
-      supriseBless: ""
+      showMusic: true,
+      supriseBless: "",
+      showResetPage: false
     }
   },
   mounted() {
-    this.database.forEach(item => {
-      preloadImg(`/static/${item.url}`)
+    this.database.forEach((item, index) => {
+      preloadImg(`/static/${item.url}`,()=>{
+        this.imgloaded++
+        if (this.imgloaded === this.database.length * 2) {
+          this.loading = false
+        }
+      })
+      preloadImg(`/static/assets/suprise/图_${index + 1}.gif`, () => {
+        this.imgloaded++
+        if (this.imgloaded === this.database.length * 2) {
+          this.loading = false
+        }
+      })
+      preloadImg("/static/assets/postcard.png",()=>{})
     })
-    this.showMusic = true
   },
   methods: {
     toMoeGirl() {
       window.open("https://zh.moegirl.org.cn/%E5%B0%8F%E5%B8%8C(%E8%99%9A%E6%8B%9FUP%E4%B8%BB)",'__blank')
     },
     clickGrid(index) {
+      if (this.showSurpirse) return
       this.currentData = database.find(item=>item.index == index)
       let url = this.database.find(item=>index === item.index).url
       this.database.filter(item=>item.url === url).forEach(item => {
@@ -157,6 +192,15 @@ export default {
       })
       localStorage.setItem('grid',JSON.stringify(this.grid))
     },
+    // before() {
+    //   let currentIndex = this.currentData.index
+    //   if (currentIndex == 0) return
+    //   if (this.grid.some)
+    // },
+    // after() {
+    //   if (this.currentData.index == this.grid.length) return
+
+    // },
     toSpace(uid) {
       if (!uid) return
       window.open(`https://space.bilibili.com/${uid}`,'__blank')
@@ -169,25 +213,32 @@ export default {
       window.open(`/static/${img}`,'__blank')
     },
     reset() {
+      this.supriseBless = ""
       this.grid = new Array(625).fill(false)
       localStorage.setItem('grid',JSON.stringify(this.grid))
     },
     happyBirthDay() {
+      // this.$refs.music.pause()
+      this.showMusic = false
       this.$refs.supriseMusic.play()
-      let bless = `选择了一幅你自己的画和你自己的歌作为礼物,这幅画让我想到那个因为整晚没有击败盖侬而哭鼻子的小希，她却不知道出了台地就单挑盖侬已经击败了多少玩家,看着这个小希的背影,我知道她有时会很脆弱，但往往最后都会打起精神，甚至眼泪还没擦干,我知道她有时会很自卑，但往往接下来会努力让自己变得更加美好，甚至还心有余悸,我知道她有时会很任性，但往往还能坚持自我勇敢前行，甚至`
+      let bless = `选择了一幅你自己的画作为礼物,这幅画让我想到那个因为整晚没有击败盖侬而哭鼻子的小希，她却不知道出了台地就单挑盖侬已经击败了多少玩家,看着这个小希的背影,我知道她有时会很脆弱，但往往最后都会打起精神，甚至眼泪还没擦干，就会像这幅画一样，坚定的勇往直前。小希，四岁生日快乐！愿你一如既往，愿你心有暖阳~`
       let splitBless = bless.split("")
       let index = 0
       let interval = setInterval(() => {
         if (!splitBless[index]) {
           clearInterval(interval)
+          this.showResetPage = true
           return
         }
         this.supriseBless += splitBless[index]
         index++
-      },250)
+      },300)
       this.showSurpirse = true
       this.grid = new Array(625).fill(true)
       localStorage.setItem('grid',JSON.stringify(new Array(625).fill(false)))
+    },
+    resetPage() {
+      location.href = location.href
     }
   },
 }
@@ -211,9 +262,32 @@ export default {
     background: rgba(0,0,0,0.12);
     -webkit-box-shadow: inset 0 0 10px rgba(0,0,0,0.2);
 }
+.loading {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  z-index: 9999;
+  background: #ee9ca7;  /* fallback for old browsers */
+  background: -webkit-linear-gradient(to right, #ffdde1, #ee9ca7);  /* Chrome 10-25, Safari 5.1-6 */
+  background: linear-gradient(to right, #ffdde1, #ee9ca7); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+}
+.loadingText {
+  font-size: 30px
+}
+.loaded {
+  font-family: 'bhwnn';
+  font-size: 30px;
+  cursor: pointer;
+}
 @font-face {
   font-family: 'bhwnn';
-  src: url('/static/assets/bhwnn.TTF');
+  src: url('https://zhzhch335-1252378763.cos.ap-beijing.myqcloud.com/bhwnn.TTF');
 }
 .navigator {
   position: absolute;
@@ -243,9 +317,35 @@ export default {
   padding: 0 50px;
   top: 30%;
   z-index: 5;
-  font-size: 30px;
+  font-size: 3vh;
   font-family: 'bhwnn';
   text-align: left;
+}
+.before, .next {
+  height: 100%;
+  font-size: 50px;
+  color:rgba(255, 255, 255, .7);
+  position: fixed;
+  top: 0;
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+  padding: 0 50px;
+}
+.before {
+  left: 0;
+}
+.next {
+  right: 0;
+}
+.resetIcon {
+  width: 50px;
+  height: 50px;
+  position: absolute;
+  left: calc(50% - 25px);
+  bottom: 10%;
+  cursor: pointer;
+  z-index: 5;
 }
 .arrow {
   position: absolute;
@@ -317,7 +417,7 @@ export default {
   padding-top: 60px;
   width: 100vw;
   height: 100vh;
-  background: #d2e9fb url("/static/assets/bg1.png");
+  background: pink url("/static/assets/bg1.png");
   background-size: 100% 100%;
   overflow: hidden;
 }
@@ -377,7 +477,7 @@ export default {
   justify-content: center;
   height: 100%;
   font-family: 'bhwnn';
-  padding: 0 50px;
+  padding: 0 20px;
   flex-grow: 1;
 }
 .blackCover .postcard .bless .content {
@@ -456,5 +556,12 @@ export default {
   position: absolute;
   right: 100px;
   bottom: 100px;
+}
+#beian {
+  position: absolute;
+  bottom: 10px;
+  left: 48%;
+  color: #ee9ca7;
+  font-family: 'bhwnn';
 }
 </style>
